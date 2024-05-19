575cc0dc9da1ff2491035d9b05bc0b4cfcf4f4ad
ashik <ashik.moinul150@gmail.com>
2024-05-20 00:08:36 +0600

    Code Added

--- lib/main.dart
+++ lib/main.dart
@@ -30,31 +30,46 @@ class Home extends StatelessWidget {
       ),
     body: Column(
         children: [
-          Flexible(                 // Flexible
-              fit: FlexFit.tight, // Flexible instead of Container
-            child:  Container(
-            width: 100,
-            height: 100,
-            color: Colors.red,
-          )
-          ),
-          Expanded(
-            //fit: FlexFit.tight,
-          child: Container(
-            width: 100,
-            height: 100,
-            color: Colors.yellow,
-          )
-        ),
-          //Container(
-        Expanded(
-            //fit: FlexFit.tight,
-            child: Container(
-              width: 100,
-              height: 100,
-              color: Colors.black,
-            )
-          )
+        //   Flexible(                 // Flexible
+        //       fit: FlexFit.tight,
+        //       flex: 1,// Flexible instead of Container
+        //     child:  Container(
+        //     width: 100,
+        //     //height: 100,
+        //     color: Colors.red,
+        //   )
+        //   ),
+        //   Expanded(
+        //     flex: 2,
+        //     //fit: FlexFit.tight,
+        //   child: Container(
+        //     width: 100,
+        //     //height: 100,
+        //     color: Colors.yellow,
+        //   )
+        // ),
+        //   //Container(
+        // Expanded(
+        //     flex: 5,
+        //     //fit: FlexFit.tight,
+        //     child: Container(
+        //       width: 100,
+        //       height: 100,
+        //       color: Colors.black,
+        //     )
+        //   )
+        //   SizedBox(
+        //     height: MediaQuery.sizeOf(context).width,
+        //     width: MediaQuery.sizeOf(context).height,
+        //     child: FractionallySizedBox(
+        //       heightFactor: 0.5,
+        //       widthFactor: 0.4,
+        //     child: Container(
+        //       color: Colors.red,
+        //     ),
+        //     )
+        //   )
+          AspectRatio(aspectRatio: 4/5, child: Container(color: Colors.green)),
       ],
 
     )
